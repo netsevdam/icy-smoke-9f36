34dbafc6805992073c0807fa9a60d9489713a43f
netsevdam <yasak73@gmail.com>
2025-03-03 05:09:30 +0300

    Update consts.ts

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "İstanbul Ulaşım Sorununa Çözüm";
+export const SITE_DESCRIPTION = "İstanbul trafiğinin sizi endişelendirmesini önleyen Esenyurt Korsan Taksi sayesinde ulaşım problemini ortadan kaldırabilir ve uygun fiyatlarla ulaşım sorununa bir çözüm bulabilirsiniz.";
